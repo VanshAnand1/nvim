f72f9d063ec84417eda46a6e68cf6aa8bdff8059
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-07-07 20:30:17 -0400

    feat: a lot

---
+++ lua/plugins/whichkey.lua
@@ -0,0 +1,22 @@
+return {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+        preset  = "helix",
+        triggers = {
+            { "<leader>", mode = { "n", "v" } },
+            { "m", mode = { "n", "v" } },
+        }
+
+    },
+    keys = {
+        {
+            "<leader>?",
+            "m?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
+}
