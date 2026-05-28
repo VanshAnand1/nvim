8c38b3867f5543af712f04e781c315f468380b1e
VanshAnand1 <vansh0711@gmail.com>
2026-05-28 15:25:33 -0400

    tab

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,22 +4,23 @@
 -- leader: Space Bar
 -- S: Shift
 -- CR: Enter
+-- Tab: Tab
 -- KEYBINDS:
 -- save and close
-vim.keymap.set({"n", "v", "i"}, "<D-s>", vim.cmd.w)
-vim.keymap.set({"n", "i", "v"}, "<D-d>", vim.cmd.q)
+vim.keymap.set({ "n", "v", "i" }, "<D-s>", vim.cmd.w)
+vim.keymap.set({ "n", "i", "v" }, "<D-d>", vim.cmd.q)
 
 -- ijkl remapping
-vim.keymap.set({"n", "v"}, "h", "i", { remap = false, desc = "Insert Mode" })
-vim.keymap.set({"n", "v"}, "H", "I", { remap = false, desc = "Insert at Line Start" })
+vim.keymap.set({ "n", "v" }, "h", "i", { remap = false, desc = "Insert Mode" })
+vim.keymap.set({ "n", "v" }, "H", "I", { remap = false, desc = "Insert at Line Start" })
 
-vim.keymap.set({"n", "v", "o"}, "j", "<Left>", { remap = false, desc = "Move Left" })
-vim.keymap.set({"n", "v", "o"}, "l", "<Right>", { remap = false, desc = "Move Right" })
-vim.keymap.set({"n", "v", "o"}, "k", "v:count1 . 'gj'", { expr = true, remap = false, desc = "Move Down" })
-vim.keymap.set({"n", "v", "o"}, "i", "v:count1 . 'gk'", { expr = true, remap = false, desc = "Move Up" })
+vim.keymap.set({ "n", "v", "o" }, "j", "<Left>", { remap = false, desc = "Move Left" })
+vim.keymap.set({ "n", "v", "o" }, "l", "<Right>", { remap = false, desc = "Move Right" })
+vim.keymap.set({ "n", "v", "o" }, "k", "v:count1 . 'gj'", { expr = true, remap = false, desc = "Move Down" })
+vim.keymap.set({ "n", "v", "o" }, "i", "v:count1 . 'gk'", { expr = true, remap = false, desc = "Move Up" })
 
-vim.keymap.set({"n", "v", "o"}, "<S-j>", "<S-Left>")
-vim.keymap.set({"n", "v", "o"}, "<S-l>", "<S-Right>")
+vim.keymap.set({ "n", "v", "o" }, "<S-j>", "<S-Left>")
+vim.keymap.set({ "n", "v", "o" }, "<S-l>", "<S-Right>")
 
 -- Window Movement
 vim.keymap.set("n", "<M-j>", "<C-w><C-h>", { desc = "Window Left" })
@@ -28,7 +29,7 @@ vim.keymap.set("n", "<M-i>", "<C-w><C-k>", { desc = "Window Up" })
 vim.keymap.set("n", "<M-l>", "<C-w><C-l>", { desc = "Window Right" })
 
 -- Neotree
-vim.keymap.set("n", "<leader>f", vim.cmd.Neotree, {desc = "Open Neotree"})
+vim.keymap.set("n", "<leader>f", vim.cmd.Neotree, { desc = "Open Neotree" })
 vim.keymap.set("n", "yp", ":let @+ = expand('%:p')<CR>") -- copy the path of the file
 
 -- Line Movement
@@ -44,8 +45,7 @@ vim.keymap.set("n", "<D-Up>", "<C-w>K")
 vim.keymap.set("n", "<D-Down>", "<C-w>J")
 vim.keymap.set("n", "<leader>v", "<C-w>v", { desc = "Vertical Split" })
 vim.keymap.set("n", "<leader>h", "<C-w>s", { desc = "Horizontal Split" })
-vim.keymap.set('n', '<Tab>', vim.cmd.bnext)
-vim.keymap.set('n', '<S-Tab>', vim.cmd.bprevious)
+vim.keymap.set("n", "<S-Tab>", vim.cmd.bnext)
 
 -- Terminal
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
@@ -53,14 +53,25 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 -- Misc
 vim.keymap.set("n", "x", '"_x') -- does not copy when deleting single character
 vim.keymap.set("n", "s", '"_s') -- does not copy when replacing single character
-vim.keymap.set({"n", "v"}, ">", ">gv") -- stays highlighted when indenting
-vim.keymap.set({"n", "v"}, "<", "<gv") -- stays highlighted when un-indenting
+vim.keymap.set({ "n", "v" }, ">", ">gv") -- stays highlighted when indenting
+vim.keymap.set({ "n", "v" }, "<", "<gv") -- stays highlighted when un-indenting
 vim.keymap.set("n", "n", "nzz") -- centres search query forwards
 vim.keymap.set("n", "N", "Nzz") -- centres search query backwards
-vim.keymap.set("n", "<Esc>", vim.cmd.nohlsearch) -- unhighlights after search
+vim.keymap.set("n", "<Tab>", function()
+	-- Start insert mode natively at the cursor position
+	vim.cmd("startinsert")
+
+	-- Feed a literal tab character to be typed immediately after entering insert mode
+	local tab = vim.api.nvim_replace_termcodes("<Tab>", true, false, true)
+	vim.api.nvim_feedkeys(tab, "n", false)
+end)
 
 -- Clipboard
 vim.keymap.set("v", "<M-y>", '"+<LeftRight>y', { remap = false, desc = "Copy selection to system clipboard" })
-vim.keymap.set("n", "<M-y>", '"+<LeftRight>y<LeftRight>y', { remap = false, desc = "Copy current line to system clipboard" })
-vim.keymap.set({"n", "v"}, "<M-p>", '"+<LeftRight>p', { remap = false, desc = "Paste from system clipboard" })
-
+vim.keymap.set(
+	"n",
+	"<M-y>",
+	'"+<LeftRight>y<LeftRight>y',
+	{ remap = false, desc = "Copy current line to system clipboard" }
+)
+vim.keymap.set({ "n", "v" }, "<M-p>", '"+<LeftRight>p', { remap = false, desc = "Paste from system clipboard" })
